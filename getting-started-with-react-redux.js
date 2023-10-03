969bf87e0c4306647a75a387600e961b59f06c27
Pedro Medeiros <116759801+a-pedro@users.noreply.github.com>
2023-10-03 12:46:02 +0200

    Add files via upload

---
+++ getting-started-with-react-redux.js
@@ -0,0 +1,13 @@
+class DisplayMessages extends React.Component {
+  // Change code below this line
+constructor (props){
+  super(props);
+  this.state = {input: "", 
+  messages: []}
+} return
+
+  // Change code above this line
+  render() {
+    return <div />
+  }
+};
